9f9f7c851b5a3cf162f5124499a65128cb554886
yutteee <yusaku_nakamura_aa@icloud.com>
2023-04-09 12:58:45 +0900

    :+1: favicon設定

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home({ posts }) {
   return (
     <div>
       <Head>
-        <title>Notion Next.js blog</title>
+        <title>jack blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -49,14 +49,9 @@ export default function Home({ posts }) {
           </div>
           <h1>Next.js blog powered by Notion API</h1>
           <p>
-            This is an example of a Next.js blog with data fetched with Notions
-            API. The data comes from{" "}
-            <a href={`https://www.notion.so/${databaseId}`}>this table</a>. Get
-            the source code on{" "}
-            <a href="https://github.com/samuelkraft/notion-blog-nextjs">
-              Github
-            </a>{" "}
-            or read{" "}
+            This is an example of a Next.js blog with data fetched with Notions API. The data comes
+            from <a href={`https://www.notion.so/${databaseId}`}>this table</a>. Get the source code
+            on <a href="https://github.com/samuelkraft/notion-blog-nextjs">Github</a> or read{" "}
             <a href="https://samuelkraft.com/blog/building-a-notion-blog-with-public-api">
               my blogpost
             </a>{" "}
@@ -67,14 +62,11 @@ export default function Home({ posts }) {
         <h2 className={styles.heading}>All Posts</h2>
         <ol className={styles.posts}>
           {posts.map((post) => {
-            const date = new Date(post.last_edited_time).toLocaleString(
-              "en-US",
-              {
-                month: "short",
-                day: "2-digit",
-                year: "numeric",
-              }
-            );
+            const date = new Date(post.last_edited_time).toLocaleString("en-US", {
+              month: "short",
+              day: "2-digit",
+              year: "numeric",
+            });
             return (
               <li key={post.id} className={styles.post}>
                 <h3 className={styles.postTitle}>
