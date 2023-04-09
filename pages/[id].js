ecf47d6ebbc019a5e330ff67590d2b45f67872d3
yutteee <yusaku_nakamura_aa@icloud.com>
2023-04-09 23:06:17 +0900

    :+1: 各記事のタイトルがogpで表示されるように

--- pages/[id].js
+++ pages/[id].js
@@ -206,7 +206,7 @@ export default function Post({ page, blocks }) {
     <div>
       <Seo
         pageTitle={"jack blog"}
-        pageDescription={"アプリ開発団体jackのメンバーズブログ"}
+        pageDescription={page.properties.Name.title}
         pagePath={`https://jackun-blog.vercel.app/${page.id}`}
         pageImg={page.cover ? page.cover.external.url : "/home.png"}
         pageImgWidth={1200}
